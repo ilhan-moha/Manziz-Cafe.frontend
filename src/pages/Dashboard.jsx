62c0653bce004cc06e71c91ad22ee558e5f5a3fe
ilhan moha <mohamudilhan99@gmail.com>
2026-08-01 02:11:14 +0300

    remove customers dashboard card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,6 @@ import {
   FaShoppingCart,
   FaDollarSign,
   FaCalendarAlt,
-  FaUsers,
 } from "react-icons/fa";
 
 import "../styles/dashboard.css";
@@ -20,35 +19,28 @@ function Dashboard() {
   orders: 0,
   revenue: 0,
   reservations: 0,
-  customers: 0,
 }); 
 
 useEffect(() => {
   const fetchStats = async () => {
     try {
       const token = localStorage.getItem("token");
-
-      const [ordersRes, reservationsRes, usersRes] = await Promise.all([
-        axios.get(`${API_URL}/orders`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }),
-        axios.get(`${API_URL}/reservations`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }),
-        axios.get(`${API_URL}/users`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }),
-      ]);
-
+       const [ordersRes, reservationsRes] = await Promise.all([
+          axios.get(`${API_URL}/orders`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(`${API_URL}/reservations`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+        ]);
+            
       const orders = ordersRes.data;
       const reservations = reservationsRes.data;
-      const users = usersRes.data;
+    
 
       const revenue = orders.reduce(
         (sum, order) => sum + order.total_price,
@@ -58,7 +50,6 @@ useEffect(() => {
       setStats({
         orders: orders.length,
         reservations: reservations.length,
-        customers: users.length,
         revenue,
       });
     } catch (err) {
@@ -105,12 +96,6 @@ useEffect(() => {
               color="#FF9800"
             />
 
-            <StatCard
-              title="Customers"
-              value={stats.customers}
-              icon={<FaUsers />}
-              color="#3F51B5"
-            />
         </div>
 
         <RecentOrders />
